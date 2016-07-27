f658c1b608f234234cad5ac70a459246e434cfb2
Helana Nosrat <helanan@gmail.com>
2016-07-27 17:43:05 -0500

    added new javascript

--- reindeer.js
+++ reindeer.js
@@ -1,1 +1,23 @@
-console.log
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+var coloredReindeer = colors + reindeer;
+
+//loop through all the reindeer//
+for (var reindeer = 0; colors < reindeer; colors++) 
+
+{
+
+
+//add name of reindeer to <div>//
+
+
+//prepend 1st array with 2nd array//
+
+//write message into page//
+var hohohoElement = document.getElementById("coloredReindeer");
+
+}
+
+//document.write("coloredReindeer");
+
+//strings are always quotes//
